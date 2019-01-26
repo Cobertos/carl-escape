5ac040e84edd9b5f7d0a06b5fb182488c94c8759
bearaxe <troaweg@gmail.com>
2019-01-25 20:28:54 -0500

    story barrel created, data/story directory created, demo added to main

---
+++ src/data/story/index.js
@@ -0,0 +1,13 @@
+import story1 from './story1.js';
+import story2 from './story2.js';
+import story3 from './story3.js';
+import story4 from './story4.js';
+import story5 from './story5.js';
+
+export default {
+  story1,
+  story2,
+  story3,
+  story4,
+  story5
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,2 @@
-console.log("We did it reddit");
+import story from '../data/story';
+console.log("We did it reddit", story.story1.scene_1.big);
